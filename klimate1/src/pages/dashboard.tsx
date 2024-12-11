5f4f99d6ea9cf0924aa0c3f4d482c55a6790820e
Pavan Kumar <siriyalapavankumar@gmail.com>
2024-12-12 01:00:30 +0530

    loading-skelton

--- klimate1/src/pages/dashboard.tsx
+++ klimate1/src/pages/dashboard.tsx
@@ -1,16 +1,39 @@
+import LoadingSkelton from "@/components/loading-skelton";
 import { Button } from "@/components/ui/button"
 import { useGeolocation } from "@/hooks/use-geolocation"
-import { RefreshCcw } from "lucide-react"
+import { AlertTriangle, MapPin, RefreshCcw } from "lucide-react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
 
 const  Dashboard = ()=>{
-    const {coordinates,error,isLoading,getLocation} = useGeolocation();
+    const {coordinates,
+        error:LocationError,
+        isLoading:LocationLoading,
+        getLocation} = useGeolocation();
     console.log(coordinates);
     const handleRefersh =() => {
         getLocation()
         if(coordinates){
             //reload weather data
         }
+    };
+    if( LocationLoading){
+        return <LoadingSkelton/>
+    }
+    if(LocationError){
+  return  (
+  <Alert variant="destructive">
+    <AlertTriangle className="h-4 w-4"/>
+  <AlertTitle>Location Error</AlertTitle>
+  <AlertDescription>
+    <p>{LocationError}</p>
+    <Button onClick={getLocation} variant={"outline"}  className="w-fit">
+        <MapPin className=" h-4 w-4 mr-2"/>
+        Enable location
+    </Button>
+  </AlertDescription>
+</Alert>
+  )
     }
     
     return (
